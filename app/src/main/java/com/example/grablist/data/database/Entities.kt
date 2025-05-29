3cbd7cce9d8b7b9e3391bbd08eca85dc3cc7cc8c
AndreaDeCarli <andreadecarli7@gmail.com>
2025-05-29 17:59:24 +0200

    defined the entities of the program

---
+++ app/src/main/java/com/example/grablist/data/database/Entities.kt
@@ -0,0 +1,56 @@
+package com.example.grablist.data.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ShopList(
+    @PrimaryKey(autoGenerate = true)
+    val shopListId: Long = 0,
+
+    @ColumnInfo
+    val title: String,
+
+    @ColumnInfo
+    val profileId: Long,
+
+    @ColumnInfo
+    val iconId: Long,
+
+    @ColumnInfo
+    val date: String
+)
+
+@Entity
+data class Product(
+    @PrimaryKey(autoGenerate = true)
+    val productId: Long = 0,
+
+    @ColumnInfo
+    val profileId: Long,
+
+    @ColumnInfo
+    val name: String,
+
+    @ColumnInfo
+    val imageUri: String,
+
+    @ColumnInfo
+    val favorite: Boolean
+)
+
+@Entity
+data class Profile(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+
+    @ColumnInfo
+    val name: String,
+)
+
+@Entity(primaryKeys = ["shopListId", "productId"])
+data class CrossRef(
+    val shopListId: Long,
+    val productId: Long
+)
